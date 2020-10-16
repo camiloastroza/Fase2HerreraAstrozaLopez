be1c304551aaec95870d20cafec0e5e1161e131a
Tomás Herrera <h.tomaseds21@gmail.com>
2020-10-16 03:46:59 -0300

    BestGame: Creando Modelos

--- juegos/catalogo/models.py
+++ juegos/catalogo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.urls import reverse
+import uuid
 
 # Create your models here.
+
+
+class Game(models.Model):
+    nombre = models.CharField(max_length=100)
+    creador = models.CharField(max_length=100)
+    descripcion = models.TextField(max_length=500)
+    cod= models.UUIDField(primary_key=True, default=uuid.uuid4)
