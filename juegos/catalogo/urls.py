7c6ebf94a05d0f7602a2d07634420073b3f6d2f7
Tomás Herrera <h.tomaseds21@gmail.com>
2020-11-01 23:46:31 -0300

    Bestgame: Creando forumularios

--- juegos/catalogo/urls.py
+++ juegos/catalogo/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from . import views
+from catalogo import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('games/', views.GameListView.as_view(), name='games'),
+    path('gamess/<int:pk>', views.GameDetailView.as_view(), name='game-detail'),
+    path('categoria/', views.categoria, name='categoria'),
+    path('mejorjuego/', views.mjrjuego, name='mjrjuego'),
+    path('retrogames/', views.retrogame, name='retro'),
+    path('formulario/', views.formulario, name='formulario'),
+]
 
-    
+
+urlpatterns+= [
+    path('game/create/', views.GameCreate.as_view(), name='game_create'),
+    path('game/<int:pk>/update/', views.GameUpdate.as_view(), name='game_update'),
+    path('game/<int:pk>/delete/', views.GameDelete.as_view(), name='game_delete'),
+
 ]
--- juegos/catalogo/views.py
+++ juegos/catalogo/views.py
@@ -1,3 +1,79 @@
 from django.shortcuts import render
+from .models import Game
+from django.views import generic
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
+def index(request):
+    num_games = Game.objects.all().count()
+
+    return render(
+        request,
+        'index.html',
+        context={'num_games': num_games},
+    )
+
+
+
+def categoria(request):
+
+    return render(
+        request,
+        'categoria.html',
+    )
+
+
+def mjrjuego(request):
+
+    return render(
+        request,
+        'mjrjuego.html',
+    )
+
+
+def retrogame(request):
+
+    return render(
+        request,
+        'retro.html',
+    )
+
+
+def formulario(request):
+
+    return render(
+        request,
+        'formulario.html',
+    )
+
+
+
+
+
+
+class GameListView(generic.ListView):
+    model = Game
+    paginate_by = 10
+
+
+
+class GameDetailView(generic.DetailView):
+    model = Game
+
+
+
+
+
+class GameCreate(CreateView):
+    model= Game
+    fields= '__all__'
+
+class GameUpdate(UpdateView):
+    model= Game
+    fields= ['Nombre','Creador','Descripción']
+
+class GameDelete(DeleteView):
+    model= Game
+    success_url = reverse_lazy('games')
