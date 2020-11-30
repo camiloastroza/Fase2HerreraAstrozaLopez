79723a20e22d6afb44875b7ceb3a16eaaef6310a
Camilo Astroza Anabalon <el.camiloawsm@gmail.com>
2020-11-29 21:11:08 -0300

    Requerimientos funcionales admin

--- juegos/catalogo/models.py
+++ juegos/catalogo/models.py
@@ -6,7 +6,28 @@ import uuid
 
 
 class Game(models.Model):
+    cod = models.CharField(max_length=8,primary_key=True)
     nombre = models.CharField(max_length=100)
     creador = models.CharField(max_length=100)
     descripcion = models.TextField(max_length=500)
-    cod= models.UUIDField(primary_key=True, default=uuid.uuid4)
+    
+
+
+
+    def get_absolute_url(self):
+	    return reverse('game_detail', args=[int(self.cod)])
+
+
+class Usuario(models.Model):
+    rut = models.CharField(max_length=8, primary_key=True, help_text="Ingrese rut sin dígito verificador.")
+    correo = models.CharField(max_length=100)
+    usuario = models.CharField(max_length=20)
+    clave = models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.usuario
+
+
+    def get_absolute_url(self):
+        return reverse('usuario_detail', args=[int(self.rut)])
